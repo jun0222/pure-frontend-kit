e22f777398956ba8602938174dbb5f275f9d1dd6
JunHatanaka <nextseason6@gmail.com>
2024-08-28 02:12:52 +0900

    feat: generateAsciiArt

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export { createClickableTable } from "./table.js";
 export { addNoZoomMetaTag, showCustomToast } from "./ui.js";
 export { createAutoSaveTextarea } from "./createAutoSaveTextarea.js";
 export { generateTimeAsciiArt, displayAsciiTime } from "./asciiNumbers.js";
+export { generateAsciiArt } from "./asciiArtGenerator.js";
